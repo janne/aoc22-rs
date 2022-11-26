8f842721592485b7ec41dddb0d93c272e2f43117
Jan Andersson <jan.andersson@gmail.com>
2022-11-26 20:21:48 +0100

    Improve template and separate from day1

--- aoc-utils/src/lib.rs
+++ aoc-utils/src/lib.rs
@@ -1,19 +1,40 @@
+use core::{fmt, result};
 use std::{error, fs, str::FromStr};
 
-pub type Done = Result<(), Box<dyn error::Error>>;
+pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
-pub fn read_chars(path: &str) -> Result<Vec<char>, Box<dyn error::Error>> {
+type PartFn<T, R> = fn(&Vec<T>) -> Result<R>;
+
+pub fn run<T, R1, R2>(input: &Vec<T>, part_a: PartFn<T, R1>, part_b: PartFn<T, R2>) -> Result<()>
+where
+    R1: fmt::Display,
+    R2: fmt::Display,
+{
+    let a = part_a(&input)?;
+    let b = part_b(&input)?;
+
+    println!("Part A: {}", a);
+    println!("Part B: {}", b);
+
+    Ok(())
+}
+
+pub fn empty<T>(_xs: &Vec<T>) -> Result<char> {
+    Ok('-')
+}
+
+pub fn read_chars(path: &str) -> result::Result<Vec<char>, Box<dyn error::Error>> {
     let contents: Vec<char> = fs::read_to_string(path)?.chars().collect();
     Ok(contents)
 }
 
-pub fn read_lines<T>(path: &str) -> Result<Vec<T>, Box<dyn error::Error>>
+pub fn read_lines<T>(path: &str) -> result::Result<Vec<T>, Box<dyn error::Error>>
 where
     T: FromStr,
     T::Err: error::Error + 'static,
 {
     let contents = fs::read_to_string(path)?;
     let lines: Vec<&str> = contents.lines().collect();
-    let lines: Result<Vec<T>, _> = lines.iter().map(|line| line.parse()).collect();
+    let lines: result::Result<Vec<T>, _> = lines.iter().map(|line| line.parse()).collect();
     Ok(lines?)
 }
--- day-template/src/main.rs
+++ day-template/src/main.rs
@@ -1,7 +1,7 @@
 use aoc_utils::{empty, read_lines, run, Result};
 
 fn main() -> Result<()> {
-    let xs = read_lines::<i32>("day1/input")?;
+    let xs = read_lines::<i32>("day-template/input")?;
 
     run(&xs, part_a, empty)
 }
