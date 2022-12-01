586d49f1c08a27359c4a411242892a2839443106
Jan Andersson <jan.andersson@gmail.com>
2022-12-01 08:31:42 +0100

    Add day1 input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,12 +1,13 @@
 use aoc_utils::{empty, read_lines, run, Result};
 
 fn main() -> Result<()> {
-    let xs = read_lines::<i32>("day1/input")?;
+    let xs = read_lines::<String>("day1/input")?;
 
     run(&xs, part_a, empty)
 }
 
-fn part_a(xs: &[i32]) -> Result<i32> {
+fn part_a(xs: &[String]) -> Result<i32> {
+    let calories: Vec<i32> = xs.into_iter().fold([].to_vec(), |acc, x| acc);
     Ok(0)
 }
 
@@ -16,8 +17,15 @@ mod tests {
 
     #[test]
     fn returns_zero() {
-        if let Ok(r) = part_a(&vec![1, 2, 3]) {
-            assert_eq!(r, 0);
+        let input: Vec<String> = vec![
+            "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "",
+            "10000",
+        ]
+        .into_iter()
+        .map(String::from)
+        .collect();
+        if let Ok(r) = part_a(&input) {
+            assert_eq!(r, 24000);
         }
     }
 }
