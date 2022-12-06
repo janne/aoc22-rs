766132bbffe4167c58eb6687ecc1921ffd0cf84b
Jan Andersson <janandersson@spotify.com>
2022-12-06 08:35:09 +0100

    Day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,22 +1,41 @@
-use std::fs;
+use std::{
+    collections::{hash_map::RandomState, HashSet},
+    fs,
+};
 
-use aoc_utils::{empty, run, Result};
+use aoc_utils::{run, Result};
 
 fn main() -> Result<()> {
     let xs: Vec<char> = fs::read_to_string("day6/input")?.chars().collect();
 
-    run(&xs, part_a, empty)
+    run(&xs, part_a, part_b)
 }
 
 fn part_a(xs: &[char]) -> Result<i32> {
-    let mut i = 3;
+    let mut i = 0;
     loop {
-        let (a, b, c, d) = (xs[i - 3], xs[i - 2], xs[i - 1], xs[i]);
-        if a != b && a != c && a != d && b != c && b != d && c != d {
-            return Ok((i + 1) as i32);
+        let cs = &xs[i..i + 4];
+        if is_distinct(cs) {
+            return Ok((i + 4) as i32);
         }
         i += 1;
     }
+}
+
+fn part_b(xs: &[char]) -> Result<i32> {
+    let mut i = 0;
+    loop {
+        let cs = &xs[i..i + 14];
+        if is_distinct(cs) {
+            return Ok((i + 14) as i32);
+        }
+        i += 1;
+    }
+}
+
+fn is_distinct(xs: &[char]) -> bool {
+    let set: HashSet<&char, RandomState> = HashSet::from_iter(xs);
+    set.len() == xs.len()
 }
 
 #[cfg(test)]
@@ -33,5 +52,19 @@ mod tests {
         assert_eq!(part_a(&c).unwrap(), 10);
         let d: Vec<char> = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".chars().collect();
         assert_eq!(part_a(&d).unwrap(), 11);
+    }
+
+    #[test]
+    fn solves_part_b() {
+        let a: Vec<char> = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".chars().collect();
+        assert_eq!(part_b(&a).unwrap(), 19);
+        let b: Vec<char> = "bvwbjplbgvbhsrlpgdmjqwftvncz".chars().collect();
+        assert_eq!(part_b(&b).unwrap(), 23);
+        let c: Vec<char> = "nppdvjthqldpwncqszvftbrmjlhg".chars().collect();
+        assert_eq!(part_b(&c).unwrap(), 23);
+        let d: Vec<char> = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".chars().collect();
+        assert_eq!(part_b(&d).unwrap(), 29);
+        let e: Vec<char> = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".chars().collect();
+        assert_eq!(part_b(&e).unwrap(), 26);
     }
 }
