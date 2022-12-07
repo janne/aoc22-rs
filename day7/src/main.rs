f28d42f710f700a6ca2e131c05108f8281da0429
Jan Andersson <janandersson@spotify.com>
2022-12-07 21:45:47 +0100

    Calculate sizes

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -18,10 +18,6 @@ impl Dir {
             parent: None,
         }
     }
-
-    pub fn add_child(&mut self, new_node: Rc<RefCell<Dir>>) {
-        self.children.push(new_node);
-    }
 }
 
 fn main() -> Result<()> {
@@ -30,38 +26,65 @@ fn main() -> Result<()> {
     run(&lines, part_a, empty)
 }
 
-fn part_a(lines: &[String]) -> Result<i32> {
+fn part_a(lines: &[String]) -> Result<u32> {
     let root = Rc::new(RefCell::new(Dir::new(".", None)));
     let mut current = Rc::clone(&root);
-
     for line in lines {
         let input: Vec<&str> = (*line).split(' ').collect();
-        println!("{:?}", input);
         match input[0] {
             "$" => match input[1] {
                 "cd" => match input[2] {
                     "/" => {
                         current = Rc::clone(&root);
                     }
-                    _ => panic!("Unknown input: {:?}", input),
+                    ".." => {
+                        let current_clone = Rc::clone(&current);
+                        current = Rc::clone(current_clone.borrow().parent.as_ref().unwrap());
+                    }
+                    a => {
+                        let current_clone = Rc::clone(&current);
+                        let current_ref = current_clone.borrow();
+                        let child = current_ref
+                            .children
+                            .iter()
+                            .find(|&d| d.borrow().name == a)
+                            .unwrap();
+                        current = Rc::clone(&child);
+                    }
                 },
                 "ls" => (),
                 _ => panic!("Unknown input: {:?}", input),
             },
             "dir" => {
-                let dir = Dir::new(input[1], None);
-                current.borrow_mut().add_child(Rc::new(RefCell::new(dir)));
+                let dir = Rc::new(RefCell::new(Dir::new(input[1], None)));
+                current.borrow_mut().children.push(Rc::clone(&dir));
+                let mut mut_dir = dir.borrow_mut();
+                mut_dir.parent = Some(Rc::clone(&current));
             }
             i => {
                 let size: u32 = i.parse().unwrap();
-                let dir = Dir::new(input[1], Some(size));
-                current.borrow_mut().add_child(Rc::new(RefCell::new(dir)));
+                let dir = Rc::new(RefCell::new(Dir::new(input[1], Some(size))));
+                current.borrow_mut().children.push(Rc::clone(&dir));
+                let mut mut_dir = dir.borrow_mut();
+                mut_dir.parent = Some(Rc::clone(&current));
             }
-            _ => panic!("Unknown input: {:?}", input),
         }
     }
 
-    Ok(0)
+    Ok(traverse(&root))
+}
+
+fn traverse(root: &Rc<RefCell<Dir>>) -> u32 {
+    let dir = root.borrow();
+    let mut sum = 0;
+    for child in &dir.children {
+        sum += traverse(child);
+    }
+
+    if dir.size.is_none() && sum <= 100_000 {
+        println!("{}", sum);
+    }
+    dir.size.unwrap_or(0) + sum
 }
 
 #[cfg(test)]
