6f2c356103805a4fc09015faccbab86af7540c57
Jan Andersson <janandersson@spotify.com>
2022-12-08 18:42:52 +0100

    Day 2 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,9 +1,9 @@
-use aoc_utils::{empty, read_lines, run, Result};
+use aoc_utils::{read_lines, run, Result};
 
 fn main() -> Result<()> {
     let xs = read_lines::<String>("day8/input")?;
 
-    run(&xs, part_a, empty)
+    run(&xs, part_a, part_b)
 }
 
 struct Trees {
@@ -41,7 +41,11 @@ fn part_a(lines: &[String]) -> Result<i32> {
 
     for y in 1..trees.height() - 1 {
         for x in 1..trees.width() - 1 {
-            if up(&trees, x, y) || right(&trees, x, y) || down(&trees, x, y) || left(&trees, x, y) {
+            if up(&trees, x, y).is_none()
+                || right(&trees, x, y).is_none()
+                || down(&trees, x, y).is_none()
+                || left(&trees, x, y).is_none()
+            {
                 count += 1;
             }
         }
@@ -50,44 +54,64 @@ fn part_a(lines: &[String]) -> Result<i32> {
     Ok(count)
 }
 
-fn left(trees: &Trees, x: usize, y: usize) -> bool {
+fn part_b(lines: &[String]) -> Result<i32> {
+    let trees = Trees::new(lines);
+
+    let mut score: usize = 0;
+
+    for y in 1..trees.height() - 1 {
+        for x in 1..trees.width() - 1 {
+            let tree_score = up(&trees, x, y).unwrap_or(y)
+                * right(&trees, x, y).unwrap_or(trees.width() - x - 1)
+                * down(&trees, x, y).unwrap_or(trees.height() - y - 1)
+                * left(&trees, x, y).unwrap_or(x);
+            if tree_score > score {
+                score = tree_score;
+            }
+        }
+    }
+
+    Ok(score as i32)
+}
+
+fn left(trees: &Trees, x: usize, y: usize) -> Option<usize> {
     let height = trees.at(x, y);
-    for i in 0..x {
+    for (distance, i) in (0..x).rev().enumerate() {
         if trees.at(i, y) >= height {
-            return false;
+            return Some(distance + 1);
         }
     }
-    return true;
+    None
 }
 
-fn right(trees: &Trees, x: usize, y: usize) -> bool {
+fn right(trees: &Trees, x: usize, y: usize) -> Option<usize> {
     let height = trees.at(x, y);
-    for i in x + 1..trees.width() {
+    for (distance, i) in (x + 1..trees.width()).enumerate() {
         if trees.at(i, y) >= height {
-            return false;
+            return Some(distance + 1);
         }
     }
-    return true;
+    None
 }
 
-fn down(trees: &Trees, x: usize, y: usize) -> bool {
+fn down(trees: &Trees, x: usize, y: usize) -> Option<usize> {
     let height = trees.at(x, y);
-    for i in y + 1..trees.height() {
+    for (distance, i) in (y + 1..trees.height()).enumerate() {
         if trees.at(x, i) >= height {
-            return false;
+            return Some(distance + 1);
         }
     }
-    return true;
+    None
 }
 
-fn up(trees: &Trees, x: usize, y: usize) -> bool {
+fn up(trees: &Trees, x: usize, y: usize) -> Option<usize> {
     let height = trees.at(x, y);
-    for i in 0..y {
+    for (distance, i) in (0..y).rev().enumerate() {
         if trees.at(x, i) >= height {
-            return false;
+            return Some(distance + 1);
         }
     }
-    return true;
+    None
 }
 
 #[cfg(test)]
@@ -105,6 +129,13 @@ mod tests {
     fn solves_part_a() {
         if let Ok(r) = part_a(&input()) {
             assert_eq!(r, 21);
+        }
+    }
+
+    #[test]
+    fn solves_part_b() {
+        if let Ok(r) = part_b(&input()) {
+            assert_eq!(r, 8);
         }
     }
 }
