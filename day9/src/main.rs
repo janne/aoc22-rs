089c03fca4469721af961c622ffc5867fde2af53
Jan Andersson <janandersson@spotify.com>
2022-12-09 17:14:34 +0100

    Day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::{cell::RefCell, collections::HashSet};
 
-use aoc_utils::{empty, read_lines, run, Result};
+use aoc_utils::{read_lines, run, Result};
 
 struct Moveable(i32, i32);
 
@@ -36,7 +36,7 @@ impl Moveable {
 fn main() -> Result<()> {
     let xs = read_lines::<String>("day9/input")?;
 
-    run(&xs, part_a, empty)
+    run(&xs, part_a, part_b)
 }
 
 fn part_a(lines: &[String]) -> Result<i32> {
@@ -57,20 +57,49 @@ fn part_a(lines: &[String]) -> Result<i32> {
     Ok(visited.len() as i32)
 }
 
+fn part_b(lines: &[String]) -> Result<i32> {
+    const COUNT: usize = 10;
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let moveables: Vec<RefCell<Moveable>> =
+        (0..COUNT).map(|_| RefCell::new(Moveable::new())).collect();
+    for line in lines {
+        let mut line_parts = line.split(' ');
+        let dir = line_parts.next().unwrap();
+        let distance = line_parts.next().unwrap().parse::<usize>().unwrap();
+        for _ in 0..distance {
+            moveables[0].borrow_mut().exec(dir);
+            for i in 1..COUNT {
+                moveables[i].borrow_mut().follow(&moveables[i - 1].borrow());
+            }
+            let tail = moveables[COUNT - 1].borrow();
+            visited.insert((tail.0, tail.1));
+        }
+    }
+    Ok(visited.len() as i32)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn input() -> Vec<String> {
-        ["R 4", "U 4", "L 3", "D 1", "R 4", "D 1", "L 5", "R 2"]
+    #[test]
+    fn solves_part_a() {
+        let input = ["R 4", "U 4", "L 3", "D 1", "R 4", "D 1", "L 5", "R 2"]
             .map(String::from)
-            .to_vec()
+            .to_vec();
+
+        let r = part_a(&input).unwrap();
+
+        assert_eq!(r, 13);
     }
 
     #[test]
-    fn returns_zero() {
-        if let Ok(r) = part_a(&input()) {
-            assert_eq!(r, 13);
-        }
+    fn solves_part_b() {
+        let input = ["R 5", "U 8", "L 8", "D 3", "R 17", "D 10", "L 25", "U 20"]
+            .map(String::from)
+            .to_vec();
+
+        let r = part_b(&input).unwrap();
+        assert_eq!(r, 36);
     }
 }
