5405833f1503206a4f9c291c90b6d8e639f30c73
Jan Andersson <janandersson@spotify.com>
2022-12-10 14:07:37 +0100

    Day 10 part 1

---
+++ day10/src/main.rs
@@ -0,0 +1,114 @@
+use aoc_utils::{empty, read_lines, run, Result};
+
+struct Cpu {
+    x: i32,
+    cycles: Vec<i32>,
+}
+
+impl Cpu {
+    fn new() -> Self {
+        Cpu {
+            x: 1,
+            cycles: Vec::new(),
+        }
+    }
+
+    fn add_cycles(&mut self, count: i32) {
+        for _ in 0..count {
+            self.cycles.push(self.x);
+        }
+    }
+
+    fn signal_strength(&self) -> i32 {
+        self.cycles[20 - 1] * 20
+            + self.cycles[60 - 1] * 60
+            + self.cycles[100 - 1] * 100
+            + self.cycles[140 - 1] * 140
+            + self.cycles[180 - 1] * 180
+            + self.cycles[220 - 1] * 220
+    }
+}
+
+enum Op {
+    Noop,
+    Addx(i32),
+}
+
+impl Op {
+    fn new(line: &str) -> Op {
+        let mut parts = line.split(' ');
+        let op = parts.next().unwrap();
+        let v = parts.next();
+        match op {
+            "noop" => Op::Noop,
+            "addx" => Op::Addx(v.unwrap().parse().unwrap()),
+            _ => panic!("Unknown op code!"),
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let lines = read_lines::<String>("day10/input")?;
+
+    run(&lines, part_a, empty)
+}
+
+fn part_a(lines: &[String]) -> Result<i32> {
+    let ops: Vec<Op> = parse(&lines);
+    let mut cpu = Cpu::new();
+
+    for op in ops {
+        match op {
+            Op::Noop => cpu.add_cycles(1),
+            Op::Addx(v) => {
+                cpu.add_cycles(2);
+                cpu.x += v;
+            }
+        }
+    }
+
+    Ok(cpu.signal_strength())
+}
+
+fn parse(input: &[String]) -> Vec<Op> {
+    input
+        .into_iter()
+        .map(|line| Op::new(line))
+        .collect::<Vec<Op>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn input() -> Vec<String> {
+        [
+            "addx 15", "addx -11", "addx 6", "addx -3", "addx 5", "addx -1", "addx -8", "addx 13",
+            "addx 4", "noop", "addx -1", "addx 5", "addx -1", "addx 5", "addx -1", "addx 5",
+            "addx -1", "addx 5", "addx -1", "addx -35", "addx 1", "addx 24", "addx -19", "addx 1",
+            "addx 16", "addx -11", "noop", "noop", "addx 21", "addx -15", "noop", "noop",
+            "addx -3", "addx 9", "addx 1", "addx -3", "addx 8", "addx 1", "addx 5", "noop", "noop",
+            "noop", "noop", "noop", "addx -36", "noop", "addx 1", "addx 7", "noop", "noop", "noop",
+            "addx 2", "addx 6", "noop", "noop", "noop", "noop", "noop", "addx 1", "noop", "noop",
+            "addx 7", "addx 1", "noop", "addx -13", "addx 13", "addx 7", "noop", "addx 1",
+            "addx -33", "noop", "noop", "noop", "addx 2", "noop", "noop", "noop", "addx 8", "noop",
+            "addx -1", "addx 2", "addx 1", "noop", "addx 17", "addx -9", "addx 1", "addx 1",
+            "addx -3", "addx 11", "noop", "noop", "addx 1", "noop", "addx 1", "noop", "noop",
+            "addx -13", "addx -19", "addx 1", "addx 3", "addx 26", "addx -30", "addx 12",
+            "addx -1", "addx 3", "addx 1", "noop", "noop", "noop", "addx -9", "addx 18", "addx 1",
+            "addx 2", "noop", "noop", "addx 9", "noop", "noop", "noop", "addx -1", "addx 2",
+            "addx -37", "addx 1", "addx 3", "noop", "addx 15", "addx -21", "addx 22", "addx -6",
+            "addx 1", "noop", "addx 2", "addx 1", "noop", "addx -10", "noop", "noop", "addx 20",
+            "addx 1", "addx 2", "addx 2", "addx -6", "addx -11", "noop", "noop", "noop",
+        ]
+        .map(String::from)
+        .to_vec()
+    }
+
+    #[test]
+    fn solves_part_a() {
+        if let Ok(r) = part_a(&input()) {
+            assert_eq!(r, 13140);
+        }
+    }
+}
