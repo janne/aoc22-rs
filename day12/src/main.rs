726993bd970115e5be424fc87d91f7ed8e438e5e
Jan Andersson <janandersson@spotify.com>
2022-12-12 17:36:56 +0100

    Day 12 part 1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,12 +1,68 @@
+use std::{cell::RefCell, collections::VecDeque};
+
 use aoc_utils::{empty, read_lines, run, Result};
 
-type Coord = (usize, usize);
+type Coord = (i32, i32);
 
 struct Position {
     coord: Coord,
     height: char,
     explored: bool,
-    from: Option<Coord>,
+    distance: i32,
+}
+struct Map {
+    goal: Coord,
+    start: Coord,
+    positions: Vec<Vec<RefCell<Position>>>,
+}
+
+impl Map {
+    fn get(&self, (x, y): Coord) -> Option<&RefCell<Position>> {
+        self.positions
+            .get(y as usize)
+            .and_then(|line| line.get(x as usize))
+    }
+
+    fn edges(&self, (x, y): Coord) -> Vec<&RefCell<Position>> {
+        let pos = &self.get((x, y)).unwrap().borrow();
+        vec![
+            self.get((x - 1, y)),
+            self.get((x, y - 1)),
+            self.get((x + 1, y)),
+            self.get((x, y + 1)),
+        ]
+        .into_iter()
+        .filter(|p| p.is_some())
+        .map(|p| p.unwrap())
+        .filter(|p| {
+            let p: &Position = &p.borrow();
+            p.height as u8 <= pos.height as u8 + 1
+        })
+        .collect()
+    }
+
+    // fn distance_from_start(&self, (x, y): Coord) -> i32 {
+    // let mut distance = 0;
+    // let mut pos: &Position = &self.get((x, y)).unwrap().borrow();
+
+    // loop {
+    //     let from = pos.from;
+    //     if from.is_none() {
+    //         return distance;
+    //     }
+    //     let new_pos = &self.get(from.unwrap()).unwrap().borrow();
+    //     pos = new_pos;
+    // }
+
+    // while pos != None {
+    //     let from = pos.from;
+    //     if from.is_none() {
+    //         pos = None;
+    //     } else {
+    //         distance += 1;
+    //     }
+    // }
+    // }
 }
 
 impl Position {
@@ -15,7 +71,7 @@ impl Position {
             coord,
             height,
             explored: false,
-            from: None,
+            distance: 0,
         }
     }
 }
@@ -26,50 +82,63 @@ fn main() -> Result<()> {
     run(&lines, part_a, empty)
 }
 
-fn find_start_goal(lines: &[String]) -> (Coord, Coord) {
-    let mut start: Coord = (0, 0);
-    let mut end: Coord = (0, 0);
-    for (y, line) in lines.iter().enumerate() {
-        for (x, c) in line.chars().enumerate() {
-            match c {
-                'S' => start = (x, y),
-                'E' => end = (x, y),
-                _ => (),
-            }
-        }
-    }
-    (start, end)
-}
-
-fn parse(lines: &[String]) -> Vec<Vec<Position>> {
-    lines
+fn parse(lines: &[String]) -> Map {
+    let mut start = (0, 0);
+    let mut goal = (0, 0);
+    let positions = lines
         .iter()
         .enumerate()
         .map(|(y, line)| {
             line.chars()
                 .enumerate()
                 .map(|(x, c)| {
-                    Position::new(
-                        (x, y),
+                    RefCell::new(Position::new(
+                        (x as i32, y as i32),
                         match c {
-                            'S' => 'a',
-                            'E' => 'z',
+                            'S' => {
+                                start = (x as i32, y as i32);
+                                'a'
+                            }
+                            'E' => {
+                                goal = (x as i32, y as i32);
+                                'z'
+                            }
                             x => x,
                         },
-                    )
+                    ))
                 })
                 .collect()
         })
-        .collect()
+        .collect();
+    Map {
+        positions,
+        start,
+        goal,
+    }
 }
 
 fn part_a(lines: &[String]) -> Result<i32> {
-    let mut map: Vec<Vec<Position>> = parse(&lines);
-    let (start, goal) = find_start_goal(&lines);
-    let mut queue: Vec<Position> = Vec::new();
-    let mut curr = start;
+    let map = parse(&lines);
+    let mut queue: VecDeque<Coord> = VecDeque::new();
+    queue.push_back(map.start);
+    while !queue.is_empty() {
+        let pos = queue.pop_front().unwrap();
+        if pos == map.goal {
+            let dist = map.get(pos).unwrap().borrow().distance;
+            return Ok(dist);
+        }
+        map.edges(pos).into_iter().for_each(|edge| {
+            if !edge.borrow().explored {
+                let mut edge_mut = edge.borrow_mut();
+                edge_mut.explored = true;
+                let distance = map.get(pos).unwrap().borrow().distance;
+                edge_mut.distance = distance + 1;
+                queue.push_back(edge_mut.coord);
+            }
+        });
+    }
 
-    Ok(0)
+    Ok(1)
 }
 
 #[cfg(test)]
@@ -85,7 +154,7 @@ mod tests {
     #[test]
     fn solves_part_a() {
         if let Ok(r) = part_a(&lines()) {
-            assert_eq!(r, 0);
+            assert_eq!(r, 31);
         }
     }
 }
